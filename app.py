23fda2a4061c5a1894c93acbbce829a947b15e68
Koya Hoshino <star.koya508@icloud.com>
2026-04-26 01:48:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
@@ -11,3 +12,7 @@ def webhook():
     data = request.json
     print("received:", data)
     return "OK", 200
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
